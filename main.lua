1aedf9c27e105559e66c036ed417bfadc2a2048c
lukeejay <lukeejay@gmail.com>
2021-03-12 23:50:48 +0000

    create main mod file
    closes #1 closes #2

---
+++ main.lua
@@ -0,0 +1,36 @@
+local mod = RegisterMod("Binding of Channel Points", 1) -- Register the mod in the API (dont change anything here, except the name)
+
+local enemiesFromQueue = 4
+
+local debugText
+
+function mod:onNewRoom()
+    mod.destroyAllRoomEntites()
+    mod.spawnEnemiesFromQueue()
+end
+
+function mod:spawnEnemiesFromQueue()
+    for i=1, enemiesFromQueue do 
+        Isaac.Spawn(EntityType.ENTITY_FLY, 0, 0, Vector(320,280), Vector(0,0), nil)
+    end
+
+end
+
+ -- remove all room enemies that aren't bosses
+function mod:destroyAllRoomEntites()
+    local entities = Isaac.GetRoomEntities()
+    
+    for i=1, #entities do
+        if entities[i]:IsVulnerableEnemy() then 
+            entities[i]:Remove()
+        end
+    end
+
+end
+
+function mod:render()
+    Isaac.RenderText(debugText, 100, 100, 255, 0, 0, 255)
+end
+
+mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.onNewRoom)
+--mod:AddCallback(ModCallbacks.MC_POST_RENDER, mod.render)
