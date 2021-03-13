abe2958e38244a8b887a5def1eba6f9c22ca6c2c
lukeejay <lukeejay@gmail.com>
2021-03-13 10:09:14 +0000

    added farting method
    closes #5

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
 local mod = RegisterMod("Binding of Channel Points", 1) -- Register the mod in the API (dont change anything here, except the name)
 
 local enemiesFromQueue = 4
-
 local debugText
 
 function mod:onNewRoom()
@@ -28,9 +27,18 @@ function mod:destroyAllRoomEntites()
 
 end
 
+function mod:fart()
+    local player = Isaac.GetPlayer(0);
+    local playerPosition = Vector(player.Position.X, player.Position.Y)
+    
+    Game().Fart( Game(), playerPosition, 1, player, 1, 0)
+
+    fartIterator = fartIterator + 1
+end
 function mod:render()
     Isaac.RenderText(debugText, 100, 100, 255, 0, 0, 255)
 end
 
 mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.onNewRoom)
 --mod:AddCallback(ModCallbacks.MC_POST_RENDER, mod.render)
+mod:AddCallback(ModCallbacks.MC_POST_FIRE_TEAR, mod.fart)
